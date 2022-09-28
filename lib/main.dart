8799302451c6fba00ba8a71e47501518e2211f3f
Anikshakya <aniklinkin@gmail.com>
2022-09-28 12:48:12 +0545

    Removed Unecessary packages

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_app_installations/firebase_app_installations.dart';
 import 'package:web_notification/home_page.dart';
 
 
@@ -31,7 +30,6 @@ void main() async{
   FirebaseMessaging.instance.getToken().then((value) {
     debugPrint(value);
   });
-  FirebaseInstallations.instance.getId().then((value) => debugPrint('id = $value'));
 }
 
 var navigatorKey = GlobalKey<NavigatorState>();
